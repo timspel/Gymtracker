3cfd52b06a0114702a891f6500986a7e111d5814
Villie99 <villie17@hotmail.com>
2023-03-22 13:58:32 +0100

    Test

--- src/controller/UserHandler.java
+++ src/controller/UserHandler.java
@@ -1,7 +1,9 @@
 package controller;
 
+import javax.swing.*;
+
 public class UserHandler {
     public static void main(String[] args) {
-
+        JOptionPane.showMessageDialog(null,"Hej");
     }
 }
