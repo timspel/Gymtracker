c89dbba853a10e11336a2cef96183dc90638f696
Alxflem <fleming.alexander99@gmail.com>
2023-03-22 14:06:35 +0100

    Modified UserHandler

--- src/controller/UserHandler.java
+++ src/controller/UserHandler.java
@@ -1,9 +1,13 @@
 package controller;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class UserHandler {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hej");
+    private ArrayList<String> users;
+
+    public UserHandler(){
+        users = new ArrayList<>();
     }
 }
