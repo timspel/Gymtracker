32fdd856e1ff56d7a20d31e93bba3f83f0e515f8
Alxflem <97131760+Alxflem@users.noreply.github.com>
2023-03-24 11:26:03 +0100

    Add files via upload

---
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/MainApp.java
@@ -0,0 +1,16 @@
+package com.gymtracker.gymtracker;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+   @Override
+   public void start(Stage stage) throws Exception {
+
+   }
+
+   public void main(String[] args){
+      launch(args);
+   }
+}
---
+++ Gymtracker/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.gymtracker.gymtracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.gymtracker.gymtracker to javafx.fxml;
+    exports com.gymtracker.gymtracker;
+}
