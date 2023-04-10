615e84636127aee7b1071d4d5f3fa6f905329151
Olof00 <ojidbratt@gmail.com>
2023-04-10 14:45:47 +0200

    Add new exercise
    Added functionality for adding new exercise. Need to be able to close down window and update list view

--- Gymtracker/src/main/java/com/gymtracker/gymtracker/controller/MainController.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/controller/MainController.java
@@ -11,8 +11,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainController {
@@ -32,6 +34,10 @@ public class MainController {
    private TextField addNameField;
    @FXML
    private ImageView addImageField;
+   @FXML
+   private TextField imageSourceField;
+   @FXML
+   private Button chooseImageButton;
    @FXML
    private ChoiceBox addMuscleGroups = new ChoiceBox();
    @FXML
@@ -107,15 +113,29 @@ public class MainController {
       }
    }
 
-   public Exercise AddNewExercise(){
+   public void chooseImage(){
+      Stage stage = new Stage();
+      FileChooser fc = new FileChooser();
+      FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+      FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+      fc.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+
+      File selectedFile = fc.showOpenDialog(stage);
+      imageSourceField.setText(selectedFile.toURI().toString());
+      Image exerciseImage = new Image(selectedFile.toURI().toString());
+      addImageField.setImage(exerciseImage);
+   }
+
+   public void AddNewExercise(){
       Exercise newExercise;
       String name = addNameField.getText();
+      String description = addDescriptionField.getText();
       Image image = addImageField.getImage();
       MuscleGroup muscleGroup = (MuscleGroup) addMuscleGroups.getSelectionModel().getSelectedItem();
-      String description = addDescriptionField.getText();
 
       newExercise = new Exercise((exercises.size() + 1 + 10),name, description, image, muscleGroup);
-
-      return newExercise;
+      System.out.println(newExercise);
+      exercises.add(newExercise);
+      exercisesList.setItems(exercises);
    }
 }
