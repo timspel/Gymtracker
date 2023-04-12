63b9aede15b0baf1cd7a26a2db9752afc05d365b
Alxflem <fleming.alexander99@gmail.com>
2023-04-12 15:08:35 +0200

    Stora ändringar
    Welcome Pane
    Ändrat namn på flertal filer och klasser
    Möjlighet att byta fönster

--- Gymtracker/src/main/java/com/gymtracker/gymtracker/MainController.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/MainController.java
@@ -4,23 +4,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller {
-   @FXML
-   private AnchorPane welcomePane;
+public class MainController implements Initializable{
    @FXML
    private Button workoutsButton;
    @FXML
@@ -31,32 +26,39 @@ public class Controller {
    private Label gymtrackerLabel;
    @FXML
    private ScrollPane scrollPane;
+   @FXML
+   private Button profileButton;
 
    private Parent workoutPane;
 
    private Parent calendarPane;
 
-   private Parent scrollContent;
-   public Controller(){ //Loads in the other frames
+   private AnchorPane scrollContent;
+   public MainController(){ //Loads in the other frames
       try {
          workoutPane = FXMLLoader.load(getClass().getResource("WorkoutPane.fxml"));
          calendarPane = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
          scrollContent = FXMLLoader.load(getClass().getResource("WelcomePane.fxml"));
-
-
       }
       catch (IOException ioe){ioe.printStackTrace();}
 }
+
    public void buttonPressed(ActionEvent event){ //Handles button presses
       if(event.getSource() == workoutsButton){
-         //Parent root = FXMLLoader.load(getClass().getResource("WorkoutPane.fxml"));
          stackPane.getChildren().removeAll();
          stackPane.getChildren().setAll(workoutPane);
       }
       if(event.getSource() == calendarButton){
-         //Parent root = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
          stackPane.getChildren().removeAll();
          stackPane.getChildren().setAll(calendarPane);
       }
+      if(event.getSource() == profileButton){
+
+      }
+   }
+
+   @Override
+   public void initialize(URL url, ResourceBundle resourceBundle) {
+      scrollPane.setContent(scrollContent);
    }
 }
