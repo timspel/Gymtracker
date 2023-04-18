34b981edeb59a5f120e8c33fb28e1ee0ed0d3cf3
Tim Do <timdo2003@gmail.com>
2023-04-18 17:20:21 +0200

    testing input data

--- Gymtracker/src/main/java/com/gymtracker/gymtracker/Database.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/Database.java
@@ -2,10 +2,37 @@ package com.gymtracker.gymtracker;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 public class Database {
 
+
+    public static void main(String[] args) {
+        addUser("john_doe", "password123", 180.5, 6.2, "https://example.com/profile/john_doe.png");
+
+
+    }
+    public static void addUser(String username, String password, double weight, double height, String profilePicture) {
+        try (Connection con = getDatabase();
+             PreparedStatement stmt = con.prepareStatement("INSERT INTO User (username, password, weight, height, profile_picture) VALUES (?, ?, ?, ?, ?)")) {
+
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+            stmt.setDouble(3, weight);
+            stmt.setDouble(4, height);
+            stmt.setString(5, profilePicture);
+
+            stmt.executeUpdate();
+
+            System.out.println("User added successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
 
     public static Connection getDatabase(){
         Connection con = null;
@@ -24,6 +51,5 @@ public class Database {
         System.out.println("Opened database successfully");
         return con;
     }
-
 
 }
--- Gymtracker/src/main/java/com/gymtracker/gymtracker/MainApplication.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/MainApplication.java
@@ -21,5 +21,7 @@ public class MainApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+
+
     }
 }
