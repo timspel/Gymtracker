9ba8e674eb356feed6ed62f18fe60e80ba499f50
Alxflem <fleming.alexander99@gmail.com>
2023-04-27 17:41:35 +0200

    Profile Tab + Nya Main Menu funktioner
    Lagt till databas hantering till Profile Taben samt lagt till Hem och logout knapp med full funktionalitet.

--- Gymtracker/src/main/java/com/gymtracker/gymtracker/MainController.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/MainController.java
@@ -4,12 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -32,6 +35,10 @@ public class MainController implements Initializable{
    private Button friendsButton;
    @FXML
    private Button exercisesButton;
+   @FXML
+   private Button homeButton;
+   @FXML
+   private Button logoutButton;
 
    private Parent workoutPane;
    private Parent calendarPane;
@@ -51,7 +58,7 @@ public class MainController implements Initializable{
       catch (IOException ioe){ioe.printStackTrace();}
 }
 
-   public void buttonPressed(ActionEvent event){ //Handles button presses
+   public void buttonPressed(ActionEvent event) throws IOException{ //Handles button presses
       if(event.getSource() == workoutsButton){
          stackPane.getChildren().removeAll();
          stackPane.getChildren().setAll(scrollPane);
@@ -72,6 +79,20 @@ public class MainController implements Initializable{
       if(event.getSource() == exercisesButton){
          stackPane.getChildren().removeAll();
          stackPane.getChildren().setAll(exercisePane);
+      }
+      if(event.getSource() == homeButton){
+         stackPane.getChildren().removeAll();
+         stackPane.getChildren().setAll(scrollPane);
+         scrollPane.setContent(scrollContent);
+      }
+      if(event.getSource() == logoutButton){
+         Parent root = FXMLLoader.load(getClass().getResource("LoginPanel.fxml"));
+         Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+         Scene scene = new Scene(root);
+         stage.setScene (scene);
+         stage.setX(0);
+         stage.setY(0);
+         stage.show();
       }
    }
    public void setParent(Parent parent){
--- Gymtracker/src/main/java/com/gymtracker/gymtracker/ProfileController.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/ProfileController.java
@@ -5,10 +5,14 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class ProfileController {
    @FXML
@@ -17,16 +21,27 @@ public class ProfileController {
    private TextArea goalsText;
    @FXML
    private Button changeButton;
+   @FXML
+   private Button editInfoButton;
+   @FXML
+   private TextField weightField;
+   @FXML
+   private TextField heightField;
+   @FXML
+   private Label lblName;
 
    private Parent profilePane;
    private MainController mainController;
-
+   private int userId;
+   private String username;
+   private double height;
+   private double weight;
    public ProfileController(MainController mainController) throws IOException {
       loadFXML();
+      userId = UserIdSingleton.getInstance().getUserId();
 
-      goalsText.setEditable(false);
-      editButton.setOnAction(l -> buttonHandler(l));
-      changeButton.setOnAction(l -> buttonHandler(l));
+      getUserInfo();
+      initComponents();
    }
 
    public Parent getParent(){
@@ -44,11 +59,74 @@ public class ProfileController {
             editButton.setText("Edit");
          }
       }
+      if(event.getSource() == editInfoButton){
+         if(editInfoButton.getText() != "Save") {
+            editInfoButton.setText("Save");
+            heightField.setEditable(true);
+            weightField.setEditable(true);
+         }
+         else {
+            editInfoButton.setText("Edit");
+            heightField.setEditable(false);
+            weightField.setEditable(false);
+         }
+      }
    }
    private void loadFXML() throws IOException{ //Loads FXML file and assigns it to parent variable.
       this.mainController = mainController;
       FXMLLoader loader = new FXMLLoader(getClass().getResource("ProfilePane.fxml"));
       loader.setController(this);
-      profilePane = (Parent) loader.load();
+      profilePane = loader.load();
+   }
+
+   private void initComponents(){
+      goalsText.setEditable(false);
+      weightField.setEditable(false);
+      heightField.setEditable(false);
+      editButton.setOnAction(l -> buttonHandler(l));
+      editInfoButton.setOnAction(l -> buttonHandler(l));
+      changeButton.setOnAction(l -> buttonHandler(l));
+      lblName.setText(username);
+      heightField.setText("" + height);
+      weightField.setText("" + weight);
+   }
+
+   private void getUserInfo(){
+      Connection con = null;
+      PreparedStatement stmt = null;
+      try {con = Database.getDatabase();
+         con.setAutoCommit(false);
+         System.out.println("Opened database successfully");
+         System.out.println(userId);
+         String sql = ("SELECT username FROM \"User\" WHERE user_id = ?");
+         stmt = con.prepareStatement(sql);
+         stmt.setInt(1, userId);
+
+         ResultSet result = stmt.executeQuery();
+         if (result.next()){
+            username = result.getString("username");
+         }
+         String query = ("SELECT weight FROM \"User\" WHERE user_id = ?");
+         stmt = con.prepareStatement(query);
+         stmt.setInt(1, userId);
+         result = stmt.executeQuery();
+         if (result.next()){
+            weight = result.getDouble("weight");
+         }
+         String query2 = ("SELECT height FROM \"User\" WHERE user_id = ?");
+         stmt = con.prepareStatement(query);
+         stmt.setInt(1, userId);
+         result = stmt.executeQuery();
+         if (result.next()){
+            height = result.getDouble("height");
+         }
+         stmt.close();
+         con.commit();
+         con.close();
+
+      } catch (Exception e) {
+         e.printStackTrace();
+         System.err.println(e.getClass().getName() + ": " + e.getMessage());
+      }
    }
 }
