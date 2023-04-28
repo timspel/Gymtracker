053923e85ae368544d3a530b1ff27f4c75b98311
Tim Do <timdo2003@gmail.com>
2023-04-28 11:38:57 +0200

    Revert "problem test"
    This reverts commit 4516796a40708c94e9dec6d02bdda019893c6d57.

--- Gymtracker/src/main/java/com/gymtracker/gymtracker/ButtonSkin.java
+++ Gymtracker/src/main/java/com/gymtracker/gymtracker/ButtonSkin.java
@@ -1,17 +1,11 @@
 package com.gymtracker.gymtracker;
 
 import javafx.scene.control.Button;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 public class ButtonSkin extends javafx.scene.control.skin.ButtonSkin {
    public ButtonSkin(Button control) {
       super(control);
-      control.setOnMouseEntered(e -> {
-         control.setStyle("-fx-background-color: #0000007a;");
-      });
-      control.setOnMouseExited(e -> {
-         control.setStyle("-fx-background-color: TRANSPARENT;");
-      });
+      control.setOnMouseEntered(e -> control.setStyle("-fx-background-color: #0000007a;"));
+      control.setOnMouseExited(e -> control.setStyle("-fx-background-color: TRANSPARENT;"));
    }
 }
