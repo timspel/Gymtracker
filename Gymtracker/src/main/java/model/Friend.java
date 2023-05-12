50d73ac3532d1f9f5411477348e65cdf118981fc
Tim Do <timdo2003@gmail.com>
2023-05-12 03:04:21 +0200

    Revert "Accept Friendrequest, remove friendrequest, populate friendslist"
    This reverts commit 2d4e72faeffec2c71114d4e9716ea7efff214f7c.

--- Gymtracker/src/main/java/model/Friend.java
+++ Gymtracker/src/main/java/model/Friend.java
@@ -1,47 +1,23 @@
 package model;
 
-public class Friend {
+import javafx.scene.image.Image;
 
+public class Friend {
 
     private int id;
     private String name;
+    private Image image;
 
-    public double getWeight() {
-        return weight;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    private double weight;
-
-    public double getHeight() {
-        return height;
-    }
-
-
-    private double height;
-    private String image;
-
-
-
-    public Friend(String name, int id, double weight, double height, String image){
+    public Friend(String name, int id){
         this.name = name;
         this.id = id;
 
-        this.weight = weight;
-        this.height = height;
-        this.image = image;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getId() {
-        return id;
-    }
 
 }
 
